eb6c722b005dd3e622f5d47d0c9998a879942582
Jose Aguilar <joseaguilar@LAUSD-Mac-44-88-7D.local>
2020-10-22 22:22:43 -0700

    First commit

---
+++ 10_21_Lesson.py
@@ -0,0 +1,40 @@
+#Lesson 10-21
+
+#Review of Lesson 10-16:
+
+#Print <- Hashtag, means comment, which means not read as code
+print("Hello World!")
+
+#Vars - Numbers, Strings, and Booleans
+name = "Mr. Aguilar"
+age = 30
+isTeacher = True
+# We can also print vars
+
+#Simple Python Math
+a = 10
+b = 15
+#print(a+b)
+#print(b-a)
+#print(a*6)
+#print(b/3)
+#a =
+#b =
+c = a + b
+#print(c)
+
+#Assigment Opps: +=, -=, *=, /=
+print(a)
+#a = a + 5
+a += 5 #Same thing as above^
+print(a)
+
+#Today:
+#Modulus -> 10/3 = 3.3333 = 3 1/3, 3 r 1 -> Modulus returns the reminder of the a / problem
+#Ex: 23 mod 7 = 2
+#Ex: 4 mod 10 = 4, 4/10 = 0.4, 0 r 4
+#Ex: 234 mod 1000 = 234 -> 
+
+#Input
+#Logic Opps
+
