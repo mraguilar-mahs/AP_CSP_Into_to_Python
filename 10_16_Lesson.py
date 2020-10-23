11869cb6127997dcec7674ffd3b3a20029d4408e
jose.aguilar3@lausd.net <Jose@265610>
2020-10-22 22:30:27 -0700

    Adding 10/16 Notes

---
+++ 10_16_Lesson.py
@@ -0,0 +1,41 @@
+#Class Notes 10/16
+#Obj: SWBAT - understand the basics of python, and programming
+#Standard:
+
+#First Line of Code:
+print("Hello World!")
+print("My name is Mr. Aguilar")
+
+#Variables: Is place holder for a value/information.
+#Data Types: Describes the type of value and properties of a variable.
+#Examples: Numbers(Int,Floats), Strings, Boolean(T/F)
+
+name = "Mr. Aguilar" # Var called name that stores the string -> "Mr. Aguilar"
+age = 23 #Var called age stores int -> 23
+isTall = True #Var called isTall stores bol -> Ture
+#isTall =/+ istall
+
+#print out our vars
+print(name)
+print(age)
+print(isTall)
+
+#Rewrite var:
+name = "Jose Aguilar"
+print(name)
+
+#Errors: Syntax error, and logical errors
+#Ex: Print("Hello")
+
+#Quick Maths: +,-,*,/
+a = 10
+b = 15
+print(a+b)
+print(b-a)
+print(a*6)
+print(b/3)
+c = a + b
+print(c)
+#Assingment Opps: +=, -=, *=, /=
+a += 10 # equiv: a = a + 10
+print(a)
