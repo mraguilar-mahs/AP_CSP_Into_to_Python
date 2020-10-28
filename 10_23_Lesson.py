baec66cf33bae0992d30c8f67feebe8eb61178ef
jose.aguilar3@lausd.net <Jose@265610>
2020-10-28 11:07:48 -0700

    Adding 10/23 & 10/28 Notes

---
+++ 10_23_Lesson.py
@@ -0,0 +1,20 @@
+
+#Lesson 1.3 Python - Class 10/23
+#Obj:
+#Standard:
+
+
+#Modulus - Reminder in a division:
+# Ex 1: 9/2 = 4 r 1
+# Ex 2: 4/10 = 0 r 4
+# modulus: % --> 9 mod 2
+print(9%2)
+print(234%1000)
+print(10%2)
+print(9%2) # <- with mod 2, check for even/odd
+# Mod can check for divisibility, if equal to 0
+
+#User Input:
+user_name = str(input("Please enter your name:"))
+print(user_name) # Name is storing the value inputed by the user from above
+
---
+++ 10_28_Lesson.py
@@ -0,0 +1,20 @@
+# Lesson 1.4 Python - Class 10/28
+# Obj:
+# Std:
+
+#User Input:
+name = input("What is you name? ") #Store the response from input -> Stores as a string (str)
+#print(name)
+age = (int)(input("Please enter your age: ")) # In this case stores age as string, not an int.
+
+# Casting:
+# Vars: String, Int, Boolean
+# Convert "10" -> 10 or 10 -> "10"
+# num_int = (int)"22" # num_int stores an int
+# num_str = (str)22 # num_str stores a str
+
+
+age += 10 # Adding 10 years to the age.
+print("Welcome " + name + " your age is " + (str)(age)) # String concatination
+# String Concat -> add strings together
+
